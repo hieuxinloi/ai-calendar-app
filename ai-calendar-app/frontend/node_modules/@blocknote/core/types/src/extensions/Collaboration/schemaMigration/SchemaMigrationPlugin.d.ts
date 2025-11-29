aba18db2cc2170b2362699bedf2cbc83910dc665
hieuxinloi <hieuhuynh.230999@gmail.com>
2025-11-30 03:00:49 +0700

    Remove .next build folder from repo

--- ai-calendar-app/frontend/node_modules/@blocknote/core/types/src/extensions/Collaboration/schemaMigration/SchemaMigrationPlugin.d.ts
+++
@@ -1,7 +0,0 @@
-import * as Y from "yjs";
-import { BlockNoteExtension } from "../../../editor/BlockNoteExtension.js";
-export declare class SchemaMigrationPlugin extends BlockNoteExtension {
-    private migrationDone;
-    static key(): string;
-    constructor(fragment: Y.XmlFragment);
-}
